89e5ee6bef0ddc91d19c4d552b12a42484b8c095
Sabina Prajapati <sabina.prajapati777@gmail.com>
2023-11-05 19:04:56 +0000

    hotels sample data added and HomeScreen modified to display hotels list

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,18 @@
+import { Row, Col } from 'react-bootstrap';
+import Hotel from '../components/Hotel';
+import hotels from '../hotels';
 
 const HomeScreen = () => {
-
   return (
     <>
-     <h1>Welcome to the Hotel booking app</h1>
+      <h1>Latest hotels</h1>
+      <Row>
+        {hotels.map((hotel) => (
+          <Col key={hotel._id} sm={12} md={6} lg={4} xl={3}>
+            <Hotel hotel={hotel} />
+          </Col>
+        ))}
+      </Row>
     </>
   );
 };
